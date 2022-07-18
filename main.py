07f4bceba009f49c5c4ec34e5e347e3aab532fad
foadfarivar <foad.farivar@curtin.edu.au>
2022-07-18 08:32:34 +0800

    added main file holder

---
+++ main.py
@@ -0,0 +1,27 @@
+#!/bin/env/python
+"""
+Description: Script for pre-processing S1 imagary.
+Original Author: foad.farivar@curtin.edu.au
+Creation Date: 2022-07-18
+"""
+
+import os
+import logging
+from unicodedata import name
+
+
+#configure logging
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
+
+
+def main():
+    pass
+
+
+if __name__=='__main__':
+    main()
