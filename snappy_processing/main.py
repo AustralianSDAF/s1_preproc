e72d9a81e921834b5924e3d3c5978f7c5840863b
Leigh <81134674+catchSheep@users.noreply.github.com>
2023-01-16 11:28:45 +0800

    Updated processing with filelist (#1)

--- snappy_processing/main.py
+++ snappy_processing/main.py
@@ -12,6 +12,7 @@ import sys
 
 import click
 import os
+from os.path import join
 import shutil
 import gc
 import logging
@@ -37,17 +38,36 @@ os.environ['LANG'] = r'C.UTF-8'
 
 @click.command()
 @click.option('--filename', default=None)
+@click.option('--filelist', default=None)
 @click.option('--shapefile', default=None)
-def main(filename, shapefile):
+def main(filename, filelist, shapefile):
     """Helper function to separate cmdline usage from python importing"""
-    process_file(filename, shapefile)
+    process_file(filename, filelist, shapefile)
 
 
-def process_file(filename, shapefile_path=None):
-    """Processes a file using input filename. If filename is None it tries to get files from a directory"""
-    # Load polygon from file
+def process_file(filename=None, filelist=None, shapefile_path=None):
+    """
+    Process a single file, and update the filelist.
+
+    Parameters
+    ----------
+    filename : str, optional
+        The name of the file to be processed.
+        One of filename or filelist should be given
+    filelist : str, optional
+        A text file that lists the filenames that will be used
+        One of filename or filelist should be given
+    shapefile_path : str, optional
+        The path to the shapefile to be used
+
+    Returns
+    -------
+    None
+
+    """
+
+    # Change config if arg present
     if(shapefile_path is not None):
-        shapefile_path = shapefile
         cfg.do_subset_from_shapefile = True
         cfg.do_subset_from_polygon = False
 
@@ -55,6 +75,7 @@ def process_file(filename, shapefile_path=None):
     log.info("Checking files and directories ...")
     pre_check = utils.pre_checks(
         filename,
+        filelist,
         cfg.raw_data_path,
         shapefile_path,
         cfg.do_subset_from_shapefile,
@@ -66,21 +87,40 @@ def process_file(filename, shapefile_path=None):
     if pre_check == 1:
         log.info("Pre-checks completed")
     else:
-        log.error("terminating execution")
+        log.error("Pre-Checks failed. Terminating execution")
         return 0
 
-    cfg.raw_data_dir = os.path.join(os.getcwd(), cfg.raw_data_path)
-    if(filename is None):
-        num_files = [f for f in os.listdir(cfg.raw_data_dir) if ".zip" in f]
+    cfg.raw_data_dir = join(os.getcwd(), cfg.raw_data_path)
+    if(filelist is not None):
+        log.info(f"Loading file list {filelist}")
+        with open(filelist, 'r') as f:
+            file_list = [l.strip('\n') for l in f.readlines() if ".zip" in l]
+        log.info(    "    \n".join(["Filelist loaded. Files to process are:", *file_list]))
     else:
-        num_files = [f for f in os.listdir(cfg.raw_data_dir) if os.path.basename(f) == os.path.basename(filename)]
-    log.info(num_files)
-    for file in num_files:
+        file_list = [f for f in os.listdir(cfg.raw_data_dir) if os.path.basename(f) == os.path.basename(filename)]
+
+    # Check if files are already processed.
+    processed_check_dir = join(cfg.final_data_path, '.processed')
+    os.makedirs(processed_check_dir, exist_ok=True)
+    processed_files = os.listdir(processed_check_dir)
+    num_files = len(file_list)
+    for fname1 in map(os.path.basename, processed_files):
+        for j, fname2 in enumerate(map(os.path.basename, file_list)):
+            if fname1 == fname2:
+                file_list.pop(num_files - j)
+
+
+    log.info(file_list)
+    for fname in file_list:
+        if(utils.check_file_processed(fname, cfg.final_data_path, zip_file_given=True)):
+            log.info("File already processed. Skipping.")
+            continue
+        # check if file exists
         gc.enable()
         gc.collect()
-        full_fname = os.path.join(cfg.raw_data_dir, file)
-        log.info("processing {}".format(file))
-        raw_product = ProductIO.readProduct(os.path.join(cfg.raw_data_dir, full_fname))
+        full_fname = join(cfg.raw_data_dir, fname)
+        log.info("processing {}".format(fname))
+        raw_product = ProductIO.readProduct(join(cfg.raw_data_dir, full_fname))
         product_name = raw_product.getName()
         input_prod = raw_product
 
@@ -127,16 +167,19 @@ def process_file(filename, shapefile_path=None):
         # writing final product
         processed_product_name = product_name + "_" + "processed"
 
-        output_path = os.path.join(cfg.final_data_path, processed_product_name)
-        output_data_dir = os.path.join(os.getcwd(), output_path)
+        output_path = join(cfg.final_data_path, processed_product_name)
+        output_data_dir = join(os.getcwd(), output_path)
 
         ProductIO.writeProduct(input_prod, output_data_dir, cfg.write_file_format)
 
         log.info("processed data saved in {}".format(output_data_dir))
 
         if cfg.do_archive_data:
-            archive_data_dir = os.path.join(os.getcwd(), cfg.archive_data_path)
-            shutil.move(cfg.raw_data_dir + "/" + file, cfg.archive_data_dir + "/" + file)
+            archive_data_dir = join(os.getcwd(), cfg.archive_data_path)
+            shutil.move(cfg.raw_data_dir + "/" + fname, cfg.archive_data_dir + "/" + fname)
+
+        # Set metadata indicating file has been processed
+        utils.create_proc_metadata(fname, cfg.final_data_path, zip_file_given=True)
 
 
 if __name__ == '__main__':
